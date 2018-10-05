c9f4ab6be215df083341cda6d9e5c51d3f730757
martin dobrev <martindobrev0@gmail.com>
2018-10-05 09:31:34 +0300

    Enhance test system usability

--- JSImpl/src/main.cpp
+++ JSImpl/src/main.cpp
@@ -4,81 +4,47 @@
 #define LOG(msg) std::cout << msg << std::endl
 #define EXECUTE_TEST(test) std::cout << #test << " "; test();
 
+#define CHECK(cond) cond ? LOG("PASS") : LOG("FAIL")
+
 // Lexer Tests
 void TestLess()
 {
 	auto tokens = Tokenize("<");
-	if (tokens.size() == 2 && tokens[0].Type == TokenType::Less)
-	{
-		LOG("PASS");
-	}
-	else
-	{
-		LOG("FAIL");
-	}
+	CHECK(tokens.size() == 2 && tokens[0].Type == TokenType::Less);
 }
 
 void TestNumber()
 {
 	auto tokens = Tokenize("213434.24");
-	if (tokens.size() == 2 && tokens[0].Type == TokenType::Number && tokens[0].Number == 213434.24)
-	{
-		LOG("PASS");
-	}
-	else
-	{
-		LOG("FAIL");
-	}
+	CHECK(tokens.size() == 2 && tokens[0].Type == TokenType::Number && tokens[0].Number == 213434.24);
 }
 
 void TestString()
 {
 	auto tokens = Tokenize("\"alabala\"");
-	if (tokens.size() == 2 && tokens[0].Type == TokenType::String && tokens[0].Lexeme == "\"alabala\"")
-	{
-		LOG("PASS");
-	}
-	else
-	{
-		LOG("FAIL");
-	}
+	CHECK(tokens.size() == 2 && tokens[0].Type == TokenType::String && tokens[0].Lexeme == "\"alabala\"");
 }
 
 void TestKeyWord()
 {
 	auto tokens = Tokenize("for");
-	if (tokens.size() == 2 && tokens[0].Type == TokenType::For)
-	{
-		LOG("PASS");
-	}
-	else
-	{
-		LOG("FAIL");
-	}
+	CHECK(tokens.size() == 2 && tokens[0].Type == TokenType::For);
 }
 
 void TestVariableDeclaration()
 {
 	auto tokens = Tokenize("var pesho = 10");
-	if (tokens.size() == 5 && tokens[0].Type == TokenType::Var
+	CHECK(tokens.size() == 5 && tokens[0].Type == TokenType::Var
 		&& tokens[1].Type == TokenType::Identifier
 		&& tokens[2].Type == TokenType::Equal
 		&& tokens[3].Type == TokenType::Number
-		&& tokens[4].Type == TokenType::Eof)
-	{
-		LOG("PASS");
-	}
-	else
-	{
-		LOG("FAIL");
-	}
+		&& tokens[4].Type == TokenType::Eof);
 }
 // Parser Tests
 void TestParseUnaryExpr()
 {
 	// TODO make actual test :D
 	auto expr = Parse(Tokenize("function pesho() { var a = 0; return a; }"));
-	expr->Print(std::cout);
 }
 
 int main()
